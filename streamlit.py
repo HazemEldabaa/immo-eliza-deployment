5cebde53d988eae96d21118034b7283904526976
HazemEl <156511160+HazemEldabaa@users.noreply.github.com>
2024-02-27 19:37:15 +0100

    fix

--- streamlit.py
+++ streamlit.py
@@ -157,27 +157,28 @@ locality = st.selectbox('Locality:', [
 if st.button('Predict Price'):
     # Prepare input data as JSON
     input_data = {
-    "nbr_frontages": "nbr_frontages",
-    "equipped_kitchen": "equipped_kitchen",
-    "nbr_bedrooms": "nbr_bedrooms",
-    "latitude": "latitude",
-    "longitude": "longitude",
-    "total_area_sqm": "total_area_sqm",
-    "surface_land_sqm": "surface_land_sqm",
-    "terrace_sqm": "terrace_sqm",
-    "garden_sqm": "garden_sqm",
-    "province": "province",
-    "heating_type": "heating_type",
-    "state_building": "state_building",
-    "property_type": "property_type",
-    "epc": "epc",
-    "locality": "locality",
-    "subproperty_type": "subproperty_type",
-    "region": "region",
-    "fl_terrace": "fl_terrace",
-    "fl_garden": "fl_garden",
-    "fl_swimming_pool": "fl_swimming_pool"
-    }
+  "nbr_frontages": nbr_frontages,
+  "equipped_kitchen": equipped_kitchen,
+  "nbr_bedrooms": nbr_bedrooms,
+  "latitude": latitude,
+  "longitude": longitude,
+  "total_area_sqm": total_area_sqm,
+  "surface_land_sqm": surface_land_sqm,
+  "terrace_sqm": terrace_sqm,
+  "garden_sqm": garden_sqm,
+  "province": province,
+  "heating_type": heating_type,
+  "state_building": state_building,
+  "property_type": property_type,
+  "epc": epc,
+  "locality": locality,
+  "subproperty_type": subproperty_type,
+  "region": region,
+  "fl_terrace": fl_terrace,
+  "fl_garden": fl_garden,
+  "fl_swimming_pool": fl_swimming_pool
+}
+
 
 
 
