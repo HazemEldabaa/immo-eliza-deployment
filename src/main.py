1f6c5a320c788fbd79df13066581f0651ff47de4
HazemEldabaa <156511160+HazemEldabaa@users.noreply.github.com>
2024-02-27 18:13:55 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
-import sys
-sys.path.append('/immo-eliza-deployment/')
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 import csv
@@ -7,9 +6,54 @@ import uvicorn
 
 from predict import predict
 from typing import Union
-from src.main_app import app
+from fastapi import FastAPI
+from pydantic import BaseModel
+import csv
+import uvicorn
 
+from predict import predict
+from typing import Union
+
+app = FastAPI()
+
+class Item(BaseModel):
+    nbr_frontages: int
+    equipped_kitchen: str
+    nbr_bedrooms: int
+    latitude: float
+    longitude: float
+    total_area_sqm: float
+    surface_land_sqm: Union[float, None]
+    terrace_sqm: float
+    garden_sqm: float
+    province: str = None
+    heating_type: str = None
+    state_building: str = None
+    property_type: str = None
+    epc: str = None
+    locality: str = None
+    subproperty_type: str = None
+    region: str = None
+    fl_terrace: bool
+    fl_garden: bool
+    fl_swimming_pool: bool
+
+
+    
+
+@app.get("/")
+async def root():
+    return {"message": "alive"}
+
+@app.post("/predict")
+async def create_item(item: Item):
+    with open("csv_file.csv", 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(item.model_dump().keys())
+        writer.writerow(item.model_dump().values())
+
+    return await predict("csv_file.csv")
 
 if __name__ == "__main__":
-    uvicorn.run("src.main_app:app", port=8000, host="0.0.0.0", reload=True)
+    uvicorn.run(app, port=8000, host="0.0.0.0")
 
