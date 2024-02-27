19a8077932c8b2ae94844d23ee62e59bdcac74f3
HazemEldabaa <156511160+HazemEldabaa@users.noreply.github.com>
2024-02-27 17:32:22 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -43,7 +43,7 @@ async def create_item(item: Item):
         writer.writerow(item.model_dump().keys())
         writer.writerow(item.model_dump().values())
 
-    return predict("csv_file.csv")
+    return await predict("csv_file.csv")
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8000, host="0.0.0.0")
