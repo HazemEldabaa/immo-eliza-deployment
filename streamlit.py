8d891f6be7dd7ddeaeceeb71174abb659988fa62
HazemEl <156511160+HazemEldabaa@users.noreply.github.com>
2024-03-03 23:52:38 +0100

    update

--- streamlit.py
+++ streamlit.py
@@ -183,7 +183,7 @@ html_content = """
     <div class = "title_div">
         <h1 class="title">Welcome to</h1>
     </div>
-    <h1 class = "immo_name" >Immo-ElizAI by VISTA</h1>
+    <h1 class = "immo_name" >IMMO-ELIZA</h1>
     <img class ="our_img" src="https://i.ibb.co/120j8dK/pngegg.png" alt="pngegg" border="0" width="75" height="50">
 </div>
 """
